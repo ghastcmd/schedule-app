d3f34994c736170d8d67650e257c8ad6fd64c45d
GhastCMD <kraken32bin@protonmail.com>
2024-11-09 14:37:14 -0300

    ended schedule page

--- frontend/app/schedules/page.tsx
+++ frontend/app/schedules/page.tsx
@@ -4,14 +4,49 @@ import Selector from "/app/components/selector";
 
 import { useState, useEffect } from "react";
 
-function Column({ dayNum, values }: { dayNum: number, values: Array }) {
-  return (
-    <div className='m-2'>
-      <h1 className=''>{dayNum}</h1>
+function Column({ weekNum, dayNum, values }: { weekNum: number, dayNum: number, values: Array }) {
+  function renderSwitch(weekNum: number) {
+    switch(weekNum) {
+      case 0:
+        return 'DOMINGO';
+        break;
+      case 1:
+        return 'SEGUNDA';
+        break;
+      case 2:
+        return 'TERÇA';
+        break;
+      case 3:
+        return 'QUARTA';
+        break;
+      case 4:
+        return 'QUINTA';
+        break;
+      case 5:
+        return 'SEXTA';
+        break;
+      case 6:
+        return 'SÁBADO';
+        break;
+    }
+  }
 
-      {values.map((v, k) => 
-        <h1 key={k} className='bg-blue-200'>{v}</h1>
-      )}
+  return (
+    <div className='border relative flex flex-col'>
+      <h1 className='text-slate-400 text-lg m-2'>
+        {renderSwitch(weekNum)}
+      </h1>
+
+      <hr />
+
+      <h1 className='m-2 text-slate-400 text-lg'>{dayNum}</h1>
+      
+      <div className='flex flex-col justify-end grow m-2'>
+        {values.map((v, k) => 
+          <h1 key={k} className='m-1 px-1 text-sky-500 rounded-md bg-sky-100'>{v}</h1>
+        )}
+      </div>
+
     </div>
   );
 }
@@ -57,14 +92,14 @@ export default function Schedule() {
       </div>
 
       <div className='flex justify-center'>
-        <div className='grid gap-2 grid-cols-7 mt-[2rem] w-11/12 h-[40rem] bg-white'>
-          <Column dayNum={weekState[0]} values={['paciente', 'paciente 2\n10:30']}/>
-          <Column dayNum={weekState[1]} values={['paciente', 'paciente 2\n10:30']}/>
-          <Column dayNum={weekState[2]} values={['paciente', 'paciente 2\n10:30']}/>
-          <Column dayNum={weekState[3]} values={['paciente', 'paciente 2\n10:30']}/>
-          <Column dayNum={weekState[4]} values={['paciente', 'paciente 2\n10:30']}/>
-          <Column dayNum={weekState[5]} values={['paciente', 'paciente 2\n10:30']}/>
-          <Column dayNum={weekState[6]} values={['paciente', 'paciente 2\n10:30']}/>
+        <div className='grid grid-cols-7 mt-[2rem] w-11/12 h-[40rem] bg-white'>
+          <Column weekNum={0} dayNum={weekState[0]} values={['paciente', 'paciente 2\n10:30']}/>
+          <Column weekNum={1} dayNum={weekState[1]} values={['paciente', 'paciente 2\n10:30']}/>
+          <Column weekNum={2} dayNum={weekState[2]} values={['paciente', 'paciente 2\n10:30']}/>
+          <Column weekNum={3} dayNum={weekState[3]} values={['paciente', 'paciente 2\n10:30']}/>
+          <Column weekNum={4} dayNum={weekState[4]} values={['paciente', 'paciente 2\n10:30']}/>
+          <Column weekNum={5} dayNum={weekState[5]} values={['paciente', 'paciente 2\n10:30']}/>
+          <Column weekNum={6} dayNum={weekState[6]} values={['paciente', 'paciente 2\n10:30']}/>
         </div>
       </div>
 
